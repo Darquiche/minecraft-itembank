f863a36a35397ce6fa42f3dfb46ef6f11812eeb0
Darquiche <darkygeek@gmail.com>
2019-07-16 17:00:54 +0200

    Fix: atm sneak

--- src/main/java/fr/darqi/itembank/EventHelper.java
+++ src/main/java/fr/darqi/itembank/EventHelper.java
@@ -9,6 +9,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -54,68 +55,36 @@ public class EventHelper implements Listener {
         }
     }
 
-    @EventHandler
-    public void onSignChange(SignChangeEvent e) {
-        Player p = e.getPlayer();
-        String deposit = e.getLine(0);
+    public void deposit(Material mat, Player p, Material curr, Sign sign) {
+        if (mat == curr && ItemBank.ibOnClick) {
+            //if in hand material = currency and OnClick feature is activated: no action.
+            return;
+        }
+        ItemStack[] inv = p.getInventory().getContents(); //get player inv content
+        int m = 0;
+        for (ItemStack anInv : inv) {
+            if (anInv != null) {
+                if (anInv.getType() == curr) { //for each item in player inv, if the item = currency, increment total value
+                    m = m + anInv.getAmount();
 
-        if (deposit.equalsIgnoreCase("[deposit]")){
-            if (p.hasPermission("itembank.create.sign.deposit")) {
-                e.setLine(0, ChatColor.BLUE + "[DEPOSIT]");
-                e.setLine(1, ChatColor.GREEN + "*****");
-                String bank = e.getLine(2);
-                if (bank != null && !bank.equals("")){
-                    e.setLine(2, ChatColor.BLUE + bank);
-                    e.setLine(3,"");
-                    p.sendMessage(ChatColor.GREEN + "Deposit sign " + bank + " successfully created.");
-                } else {
-                    e.setLine(2,"");
-                    e.setLine(3,"");
-                    p.sendMessage(ChatColor.GREEN + "Deposit sign successfully created.");
                 }
-            } else {
-                e.setLine(0, ChatColor.RED + "Forbidden.");
-                p.sendMessage(ChatColor.RED + "You need: itembank.create.sign.deposit");
             }
         }
-        if (deposit.equalsIgnoreCase("[withdraw]")){
-            if (p.hasPermission("itembank.create.sign.withdraw")) {
-                String valFirst = e.getLine(1);
-                String valSecond = e.getLine(2);
-                String valThird = e.getLine(3);
-                try {
-                    int first = Integer.parseInt(valFirst);
-                    int second = Integer.parseInt(valSecond);
-                    int third = Integer.parseInt(valThird);
-
-                    if (first != 0 && second != 0 && third != 0) {
-                        e.setLine(0, ChatColor.BLUE + "[WITHDRAW]");
-                        e.setLine(1, ChatColor.AQUA + valFirst);
-                        e.setLine(2, ChatColor.AQUA + valSecond);
-                        e.setLine(3, ChatColor.AQUA + valThird);
-
-                        p.sendMessage(ChatColor.GREEN + "Withdraw sign successfully created.");
-                    } else {
-                        e.setLine(0, ChatColor.RED + "Error.");
-                        p.sendMessage(ChatColor.RED + "Tnteger(s) must be > 0.");
-                    }
-
-                } catch (NumberFormatException ex) {
-                    e.setLine(0, ChatColor.RED + "Error.");
-                    p.sendMessage(ChatColor.RED + "Please enter an integer ( > 0 ) on each line.\ne.g [withdraw] on first line, then 10, then 100, and 1000.");
-                }
-            } else {
-                e.setLine(0, ChatColor.RED + "Forbidden.");
-                p.sendMessage(ChatColor.RED + "You need: itembank.create.sign.withdraw");
-            }
+        if (m == 0) {
+            //if no item, no action
+            return;
         }
-        if (deposit.equalsIgnoreCase("[atm]")){
-            if (p.hasPermission("itembank.create.sign.atm")) {
-                p.sendMessage(ChatColor.GREEN + "ATM sign successfully created.");
-            } else {
-                e.setLine(0, ChatColor.RED + "Forbidden.");
-                p.sendMessage(ChatColor.RED + "You need: itembank.create.sign.atm");
-            }
+        p.getInventory().remove(curr); //remove the items
+        EconomyResponse r = ItemBank.econ.depositPlayer(p, m); //and deposit the value in player's vault
+        String bank = sign.getLine(2); //get optional bank's name
+        if (!r.transactionSuccess()) {
+            p.sendMessage(String.format(ChatColor.RED + "An error occured: %s", r.errorMessage));
+            return;
+        }
+        if (bank != null && !bank.equals("")) {
+            p.sendMessage(String.format(ChatColor.AQUA + "You have just deposited all your %s in " + bank + ".\nDeposited: " + ChatColor.RESET + "%s\n" + ChatColor.AQUA + "Your account: " + ChatColor.RESET + "%s", ItemBank.ibCurrency, ItemBank.econ.format(r.amount), ItemBank.econ.format(r.balance)));
+        } else {
+            p.sendMessage(String.format(ChatColor.AQUA + "You have just deposited all your %s.\nDeposited: " + ChatColor.RESET + "%s\n" + ChatColor.AQUA + "Your account: " + ChatColor.RESET + "%s", ItemBank.ibCurrency, ItemBank.econ.format(r.amount), ItemBank.econ.format(r.balance)));
         }
     }
 
@@ -146,7 +115,7 @@ public class EventHelper implements Listener {
 
     @EventHandler
     public void onSignClick(PlayerInteractEvent event) {
-        Action a = event.getAction();
+        Action a = event.getAction(); //get action
         if (a != Action.RIGHT_CLICK_BLOCK) {
             //no action if it's not a right click on a block.
             return;
@@ -155,79 +124,128 @@ public class EventHelper implements Listener {
             //no action if config says NO!
             return;
         }
-        Material mat = event.getMaterial();
-        Player p = event.getPlayer();
-        Material curr = Material.matchMaterial(ItemBank.ibCurrency);
-        Block b = event.getClickedBlock();
+        Material mat = event.getMaterial(); //get player in hand material
+        Player p = event.getPlayer(); //get player
+        Material curr = Material.matchMaterial(ItemBank.ibCurrency); //convert string to material
+        Block b = event.getClickedBlock(); //get clicked block
         if (isWallSignPost(b.getType())) {
-            Sign sign = (Sign) b.getState();
+            Sign sign = (Sign) b.getState(); //get sign object
             if (ChatColor.stripColor(sign.getLine(0)).equals("[DEPOSIT]")) {
                 //deposit sign
-                if (p.hasPermission("itembank.use.sign.deposit")) {
-                    if (mat == curr) {
-                        return;
-                    }
-                    ItemStack[] inv = p.getInventory().getContents();
-                    int total = 0;
-                    int m = 0;
-                    for (int i = 0; i < inv.length; i++) {
-                        if (inv[i] != null) {
-                            if (inv[i].getType() == curr) {
-                                m = m + inv[i].getAmount();
-
-                            }
-                            total = total + 1;
-                        }
-                    }
-                    if (m == 0) {
-                        return;
-                    }
-                    p.getInventory().remove(curr);
-                    EconomyResponse r = ItemBank.econ.depositPlayer(p, m);
-                    String bank = sign.getLine(2);
-                    if (!r.transactionSuccess()) {
-                        p.sendMessage(String.format(ChatColor.RED + "An error occured: %s", r.errorMessage));
-                        return;
-                    }
-                    if (bank != null && !bank.equals("")) {
-                        p.sendMessage(String.format(ChatColor.AQUA + "You have just deposited all your %s in " + bank + ".\nDeposited: " + ChatColor.RESET + "%s\n" + ChatColor.AQUA + "Your account: " + ChatColor.RESET + "%s", ItemBank.ibCurrency, ItemBank.econ.format(r.amount), ItemBank.econ.format(r.balance)));
-                    } else {
-                        p.sendMessage(String.format(ChatColor.AQUA + "You have just deposited all your %s.\nDeposited: " + ChatColor.RESET + "%s\n" + ChatColor.AQUA + "Your account: " + ChatColor.RESET + "%s", ItemBank.ibCurrency, ItemBank.econ.format(r.amount), ItemBank.econ.format(r.balance)));
-                    }
-                } else {
-                    p.sendMessage(ChatColor.RED + "You need: itembank.use.sign.deposit");
-                }
+                deposit(mat, p, curr, sign);
             } else if (ChatColor.stripColor(sign.getLine(0)).equals("[WITHDRAW]")) {
                 //withdraw sign
                 if (p.hasPermission("itembank.use.sign.withdraw")) {
-                    if (p.isSneaking()) {
-                        //change value
-                        String strFirst = sign.getLine(1);
-                        String strSecond = sign.getLine(2);
-                        String strThird = sign.getLine(3);
-                        if (strFirst.startsWith(String.valueOf(ChatColor.RED))){
-                            sign.setLine(1, ChatColor.AQUA + ChatColor.stripColor(strFirst));
-                            sign.setLine(2, ChatColor.RED + ChatColor.stripColor(strSecond));
-                        } else if (strSecond.startsWith(String.valueOf(ChatColor.RED))){
-                            sign.setLine(2, ChatColor.AQUA + ChatColor.stripColor(strSecond));
-                            sign.setLine(3, ChatColor.RED + ChatColor.stripColor(strThird));
-                        } else if (strThird.startsWith(String.valueOf(ChatColor.RED))){
-                            sign.setLine(3, ChatColor.AQUA + ChatColor.stripColor(strThird));
-                            sign.setLine(1, ChatColor.RED + ChatColor.stripColor(strFirst));
+                    //TODO: fix gui
+                    //ItemBank.menu.show(p);
+                }
+            } else if (ChatColor.stripColor(sign.getLine(0)).equals("[ATM]")) {
+                //atm sign
+                if (p.hasPermission("itembank.use.sign.atm")) {
+                    if (p.isSneaking()){
+                        //change atm state
+                        if (ChatColor.stripColor(sign.getLine(1)).equals("[DEPOSIT]")) {
+                            sign.setLine(1, ChatColor.BLUE + "[WITHDRAW]");
                         } else {
-                            sign.setLine(1, ChatColor.RED + ChatColor.stripColor(strFirst));
+                            sign.setLine(1, ChatColor.BLUE + "[DEPOSIT]");
                         }
+                        sign.update();
                     } else {
-                        String test = sign.getLine(1);
-                        if (test.startsWith(String.valueOf(ChatColor.AQUA))){
-                            String strip = ChatColor.stripColor(test);
-                            p.sendMessage(ChatColor.stripColor(test));
-                            //log.info(ChatColor.stripColor(test));
-                            sign.setLine(1, ChatColor.RED + strip);
-                        } else {
-                            p.sendMessage("false");
+                        //use atm
+                        if (ChatColor.stripColor(sign.getLine(1)).equals("[DEPOSIT]")) {
+                            if (p.hasPermission("itembank.use.sign.deposit")) {
+                                deposit(mat, p, curr, sign);
+                            } else {
+                                p.sendMessage(ChatColor.RED + "You need permission: itembank.use.sign.deposit");
+                            }
+                        } else if (ChatColor.stripColor(sign.getLine(1)).equals("[WITHDRAW]")) {
+                            if (p.hasPermission("itembank.use.sign.withdraw")) {
+                                //TODO: fix gui
+                                //menu.show(p);
+                            }
                         }
                     }
+                } else {
+                    p.sendMessage(ChatColor.RED + "You need permission: itembank.use.sign.atm");
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onSignChange(SignChangeEvent e) {
+        Player p = e.getPlayer();
+        String deposit = e.getLine(0);
+
+        if (deposit.equalsIgnoreCase("[deposit]")){
+            if (p.hasPermission("itembank.create.sign.deposit")) {
+                e.setLine(0, ChatColor.BLUE + "[DEPOSIT]");
+                e.setLine(1, ChatColor.GREEN + "*****");
+                String bank = e.getLine(2);
+                if (bank != null && !bank.equals("")){
+                    e.setLine(2, ChatColor.BLUE + bank);
+                    e.setLine(3,"");
+                    p.sendMessage(ChatColor.GREEN + "Deposit sign " + bank + " successfully created.");
+                } else {
+                    e.setLine(2,"");
+                    e.setLine(3,"");
+                    p.sendMessage(ChatColor.GREEN + "Deposit sign successfully created.");
+                }
+            } else {
+                e.setLine(0, ChatColor.RED + "Forbidden.");
+                p.sendMessage(ChatColor.RED + "You need permission: itembank.create.sign.deposit");
+            }
+        }
+        if (deposit.equalsIgnoreCase("[withdraw]")){
+            if (p.hasPermission("itembank.create.sign.withdraw")) {
+                e.setLine(0, ChatColor.BLUE + "[WITHDRAW]");
+                e.setLine(1, ChatColor.GREEN + "*****");
+                String bank = e.getLine(2);
+                if (bank != null && !bank.equals("")){
+                    e.setLine(2, ChatColor.BLUE + bank);
+                    e.setLine(3,"");
+                    p.sendMessage(ChatColor.GREEN + "Withdraw sign " + bank + " successfully created.");
+                } else {
+                    e.setLine(2,"");
+                    e.setLine(3,"");
+                    p.sendMessage(ChatColor.GREEN + "Withdraw sign successfully created.");
+                }
+            } else {
+                e.setLine(0, ChatColor.RED + "Forbidden.");
+                p.sendMessage(ChatColor.RED + "You need permission: itembank.create.sign.withdraw");
+            }
+        }
+        if (deposit.equalsIgnoreCase("[atm]")){
+            if (p.hasPermission("itembank.create.sign.atm")) {
+                e.setLine(0, ChatColor.DARK_PURPLE + "[ATM]");
+                e.setLine(1, ChatColor.BLUE + "[DEPOSIT]");
+                String bank = e.getLine(2);
+                if (bank != null && !bank.equals("")){
+                    e.setLine(2, ChatColor.BLUE + bank);
+                    e.setLine(3,"");
+                    p.sendMessage(ChatColor.GREEN + "ATM sign " + bank + " successfully created.");
+                } else {
+                    e.setLine(2,"");
+                    e.setLine(3,"");
+                    p.sendMessage(ChatColor.GREEN + "ATM sign successfully created.");
+                }
+            } else {
+                e.setLine(0, ChatColor.RED + "Forbidden.");
+                p.sendMessage(ChatColor.RED + "You need permission: itembank.create.sign.atm");
+            }
+        }
+    }
+
+    @EventHandler
+    public void onBlockDamage(BlockBreakEvent e) {
+        Block b = e.getBlock();
+        Player p = e.getPlayer();
+        if (isWallSignPost(b.getType()))  {
+            Sign sign = (Sign) b.getState(); //get sign object
+            if (ChatColor.stripColor(sign.getLine(0)).equals("[ATM]") || ChatColor.stripColor(sign.getLine(0)).equals("[DEPOSIT]") || ChatColor.stripColor(sign.getLine(0)).equals("[WITHDRAW]")) {
+                if (!p.hasPermission("itembank.break.sign")) {
+                    e.setCancelled(true);
+                    p.sendMessage(ChatColor.RED + "You need permission: itembank.break.sign");
                 }
             }
         }
